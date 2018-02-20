45f1960599626e2aa3936f64f6554ca8418eee30
jtb1137 <jtb1137@gmail.com>
2018-02-20 12:12:09 -0500

    minor clean up

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+
+    # Add ingredients to recipe & find or create new ingredient
+
 end
